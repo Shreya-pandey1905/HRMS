4609ca4df8cecf13fa051cda64f88d271c8775d6
Ankush pal <155830977+ankush9769@users.noreply.github.com>
2026-07-31 00:48:21 +0530

    add employee feature added

---
+++ src/main/java/Servlets/AddEmployee.java
@@ -0,0 +1,56 @@
+package Servlets;
+
+import Dao.UserDao;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.User;
+import utils.PasswordUtil;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/AddEmployee")
+public class AddEmployee extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String department = req.getParameter("department");
+        String password = name+"123";
+        String hashpassword = PasswordUtil.hash(password);
+        try {
+            boolean iscreated = UserDao.addEmployee(name,email,department,hashpassword);
+            PrintWriter out = resp.getWriter();
+            if(iscreated){
+//                out.println(
+//                        "<html>" +
+//                                "<body style='background-color:#f8f9fa;'>" +
+//                                "<h1 style='color:green; text-align:center; margin-top:100px;'>Employee Added Successfully!</h1>" +
+//                                "</body>" +
+//                                "</html>"
+//                );
+                resp.sendRedirect("employee");
+            }else{
+                out.println(
+                        "<html>" +
+                                "<body style='background-color:#f8f9fa;'>" +
+                                "<h1 style='color:red; text-align:center; margin-top:100px;'>Employee Could Not Be Added!</h1>" +
+                                "</body>" +
+                                "</html>"
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
---
+++ src/main/java/Servlets/EmployeeServlet.java
@@ -0,0 +1,32 @@
+package Servlets;
+
+import Dao.UserDao;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.User;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+@WebServlet("/employee")
+public class EmployeeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> employees = null;
+        try {
+            employees = UserDao.getAllUser();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        req.setAttribute("employees", employees);
+        RequestDispatcher rd = req.getRequestDispatcher("Employee.jsp");
+        rd.forward(req,resp);
+    }
+
+}
