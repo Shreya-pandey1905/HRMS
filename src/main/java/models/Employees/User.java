625e4a953353a74e10225658f02652da3694f661
Akanksha Patel <akanksha21patel@gmail.com>
2026-09-08 21:19:40 +0530

    Complete Training module
    A

--- src/main/java/models/Employees/User.java
+++ src/main/java/models/Employees/User.java
@@ -1,4 +1,35 @@
 package models.Employees;
 
 public class User {
+
+    private int userId;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
