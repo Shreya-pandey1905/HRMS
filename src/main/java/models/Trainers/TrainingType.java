00e20b73fb7d4a523755ff70e5278d0c1b694697
akank85 <130525889+akank85@users.noreply.github.com>
2026-09-10 18:48:49 +0530

    Update TrainingType.java

--- src/main/java/models/Trainers/TrainingType.java
+++ src/main/java/models/Trainers/TrainingType.java
@@ -1,4 +1,52 @@
 package models.Trainers;
 
 public class TrainingType {
+
+    private int trainingTypeId;
+    private String trainingTypeName;
+    private String description;
+    private String status;
+
+    public TrainingType() {
+    }
+
+    public TrainingType(int trainingTypeId, String trainingTypeName,
+                        String description, String status) {
+        this.trainingTypeId = trainingTypeId;
+        this.trainingTypeName = trainingTypeName;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getTrainingTypeId() {
+        return trainingTypeId;
+    }
+
+    public void setTrainingTypeId(int trainingTypeId) {
+        this.trainingTypeId = trainingTypeId;
+    }
+
+    public String getTrainingTypeName() {
+        return trainingTypeName;
+    }
+
+    public void setTrainingTypeName(String trainingTypeName) {
+        this.trainingTypeName = trainingTypeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
